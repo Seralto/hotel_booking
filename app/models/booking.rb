7f45d48d8d8096555c0af6e1f0b77c11935bede8
Sérgio Toledo <sergio.x.toledo.-nd@espn.com>
2016-06-27 00:39:59 -0300

    Fixed booking validation per date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ActiveRecord::Base
-  validates :user_id, uniqueness: { scope: [:place_id, :checkin], message: 'already booked this hotel on this date.' }
+  validates :user_id, uniqueness: { scope: [:checkin], message: 'already has a booking on this date.' }
   validates :place_id, :hotel, :city, :checkin, :user_id, presence: true
 
   belongs_to :user
