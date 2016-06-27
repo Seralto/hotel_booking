f0d6032c9c946033465eb8d40622862fa44e3c4e
Sérgio Toledo <sergio.x.toledo.-nd@espn.com>
2016-06-27 16:37:24 -0300

    Added validation to checkin date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,12 @@
 class Booking < ActiveRecord::Base
   validates :user_id, uniqueness: { scope: [:checkin], message: 'already has a booking on this date.' }
   validates :place_id, :hotel, :city, :checkin, :user_id, presence: true
+  validate :checkin_greater_than_today
 
   belongs_to :user
+
+  def checkin_greater_than_today
+    errors.add(:checkin, 'date must be greater than today.') if checkin < Date.today
+    errors.blank?
+  end
 end
