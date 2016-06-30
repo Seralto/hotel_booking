058930be36da7adc8257a659e3b66bd1038626f5
Sérgio Toledo <sergio.x.toledo.-nd@espn.com>
2016-06-30 12:41:49 -0300

    Added enconding

--- app/api/hotel_booking/v1.rb
+++ app/api/hotel_booking/v1.rb
@@ -1,6 +1,7 @@
 module HotelBooking
   class V1 < Grape::API
     version 'v1', using: :path, vendor: 'hotel_booking'
+    content_type :json, 'application/json; charset=UTF-8'
     prefix :api
     format :json
 
